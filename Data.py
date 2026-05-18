7db3b15c24a312a8b2e6d6e7d240c4232d661877
usffish <ismailj@usf.edu>
2026-05-18 08:24:20 -0400

    loaded datasets and trained the model for gsm

---
+++ Data.py
@@ -0,0 +1,27 @@
+from datasets import load_dataset
+
+# import datasets
+gsm = load_dataset("gsm8k", "main")
+gpqa = load_dataset("Idavidrein/gpqa", "gpqa_main")
+'''
+# Explore
+print ("GSM8K")
+for ex in gsm["test"].select(range(3)):
+    print("Q:", ex["question"])
+    print("A:", ex["answer"])
+    print()
+
+print("GPQA")
+for ex in gpqa["train"].select(range(3)):
+    print("Q:", ex["Question"][:200])
+    print("Correct:", ex["Correct Answer"][:100])
+    print("Subdomain:", ex["Subdomain"])
+    print()
+'''
+# Format dataset
+def format_gsm(data):
+    return {"text": f"<|im_start|>user\n{data['question']}<|im_end|>\n"
+                    f"<|im_start|>assistant\n{data['answer']}<|im_end|>"}
+
+fgsm = gsm.map(format_gsm)
+# print(fgsm.column_names)
--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-from transformers import pipeline,AutoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from peft import LoraConfig, TaskType
+from trl import SFTTrainer, SFTConfig
+from Data import fgsm, gpqa
+import torch
 
-qwen_config = AutoConfig.from_pretrained("Qwen/Qwen2.5-1.5B")
-print(qwen_config)
-'''
-question_answerer = pipeline("text-generation", model="Qwen/Qwen2.5-1.5B")
-result=question_answerer("question:Where do I work?\n"
-                         "Context:My name is Sylvain and I work at Hugging Face in Brooklyn\n"
-                         "Answer:"
-                         )
-print(result)
-'''
+# load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B", dtype=torch.float32, low_cpu_mem_usage=False).to("mps")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B")
+tokenizer.pad_token = tokenizer.eos_token
+
+# Wrap with LoRA
+lora = LoraConfig(
+    r=16, lora_alpha=32,
+    target_modules=["q_proj","k_proj","v_proj","o_proj"],
+    lora_dropout=0.05, bias="none",
+    task_type=TaskType.CAUSAL_LM
+)
+
+#model = get_peft_model(model, lora)
+#model.print_trainable_parameters()
+
+# config
+args = SFTConfig(
+    output_dir="./output",
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    learning_rate=2e-4,
+    warmup_steps=50,
+    lr_scheduler_type="cosine",
+    logging_steps=10,
+    save_steps=100,
+    max_length=2000,
+    fp16=False, bf16=False,
+    dataloader_pin_memory=False,
+)
+
+# Train
+mps_trainer = SFTTrainer(
+    model=model,
+    args=args,
+    train_dataset = fgsm["train"],
+    peft_config=lora,
+    processing_class=tokenizer,
+)
+
+mps_trainer.train()
+model.save_pretrained("./tuned-qwen-gsm")
